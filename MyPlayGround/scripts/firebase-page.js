68a6305a45a4c214c10a61829aa8429a499be044
MiyazakiRyuta <ryuta_miyazaki@ot.olmpus.co.jp>
2017-11-07 11:28:19 +0900

    FireBaseにgooglehome用データビュー追加

--- MyPlayGround/scripts/firebase-page.js
+++ MyPlayGround/scripts/firebase-page.js
@@ -8,54 +8,79 @@
 };
 firebase.initializeApp(config);
 
-// DB値の取得
+firebase.database().ref("googlehome").on('value', function (snapshot) {
+    let date = new Date();
+    let datetimeText = "" + date.getFullYear() + date.getMonth() + date.getDate() + date.getHours() + date.getMinutes() + date.getSeconds();
+    firebase.database().ref("google-home/" + datetimeText).set(snapshot.val());
+    
+    setTimeout(function () {
+        // データ削除
+        firebase.database().ref("googlehome").set(null);
+    },0)
+});
+
+firebase.database().ref("google-home").on('value', function (snapshot) {
+    let dataList = snapshot.val();
+    if (dataList) {
+        var dataListKeys = Object.keys(dataList);
+
+        let ul = document.getElementById("view-ul");
+        ul.innerHTML = "";
+
+        for (var i = 0, len = dataListKeys.length; i < len; i++) {
+            let value = dataList[dataListKeys[i]];
+
+            let li = document.createElement('li');
+            li.className = "mdl-list__item";
+
+            let span = document.createElement('span');
+            span.className = "mdl-list__item-primary-content";
+            span.innerHTML = value;
+
+            li.appendChild(span);
+            ul.appendChild(li);
+        }
+    }
+});
+
 firebase.database().ref("iot-hue").on('value', function (snapshot) {
     let dataList = snapshot.val();
-    var dataListKeys = Object.keys(dataList);
+    if (dataList) {
+        var dataListKeys = Object.keys(dataList);
 
-    let table = document.getElementById("view-table");
-    let tdbody = table.getElementsByTagName("tbody")[0];
+        let table = document.getElementById("view-table");
+        let tdbody = table.getElementsByTagName("tbody")[0];
+        tdbody.innerHTML = "";
 
-    //let ul = document.getElementById("view-ul");
-    //ul.innerHTML = "";
 
-    for (var i = 0, len = dataListKeys.length; i < len; i++) {
-        let dataObj = dataList[dataListKeys[i]];
 
-        let tr = document.createElement('tr');
-        let tdKey = document.createElement('td');
-        tdKey.innerHTML = Object.keys(dataObj)[0];
-        tdKey.className = "mdl-data-table__cell--non-numeric";
-        tr.appendChild(tdKey);
+        for (var i = 0, len = dataListKeys.length; i < len; i++) {
+            let dataObj = dataList[dataListKeys[i]];
 
-        let tdValue = document.createElement('td');
-        tdValue.innerHTML = dataObj[Object.keys(dataObj)[0]];
-        tdValue.className = "mdl-data-table__cell--non-numeric";
-        tr.appendChild(tdValue);
+            let tr = document.createElement('tr');
+            let tdKey = document.createElement('td');
+            tdKey.innerHTML = Object.keys(dataObj)[0];
+            tdKey.className = "mdl-data-table__cell--non-numeric";
+            tr.appendChild(tdKey);
 
-        tdbody.appendChild(tr);
+            let tdValue = document.createElement('td');
+            tdValue.innerHTML = dataObj[Object.keys(dataObj)[0]];
+            tdValue.className = "mdl-data-table__cell--non-numeric";
+            tr.appendChild(tdValue);
 
-        // リスト表示
-        //let li = document.createElement('li');
-        //li.className = "mdl-list__item";
+            tdbody.appendChild(tr);
 
-        //let span = document.createElement('span');
-        //span.className = "mdl-list__item-primary-content";
-        //span.innerHTML = Object.keys(dataObj)[0] + " : " + dataObj[Object.keys(dataObj)[0]];
 
-        //li.appendChild(span);
-
-        //ul.appendChild(li);
+        }
     }
 });
 
 $("#registration").click(function () {
     let obj = {};
+    obj[$("#key").val()] = $("#value").val();
 
     let date = new Date();
     let datetimeText = "" + date.getFullYear() + date.getMonth() + date.getDate() + date.getHours() + date.getMinutes() + date.getSeconds();
-
-    obj[$("#key").val()] = $("#value").val();
 
     firebase.database().ref("iot-hue/" + datetimeText).set(obj);
 });
